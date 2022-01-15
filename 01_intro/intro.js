40f3b140a8c6542f69bbc57c003c3288869432ca
UZBase <sharofiddinov.07@mail.ru>
2022-01-15 09:37:48 +0300

    add

--- 01_intro/intro.js
+++ 01_intro/intro.js
@@ -11,4 +11,6 @@ function expect(value) {
 }
 const sum = (a, b) => a + b
 const nativeNull = () => null
-expect(sum(41, 1)).toBe(43)
+module.exports = { sum, nativeNull }
+
+// expect(sum(41, 1)).toBe(43)
---
+++ 01_intro/intro.test.js
@@ -0,0 +1,3 @@
+const {sum} = require('./intro')
+
+test()
